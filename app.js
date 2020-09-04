514c7ed4a11cb25157148a81e9e6c82601230186
ghozimahdi <profghozimahdi@gmail.com>
2020-09-04 14:51:59 +0700

    change port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/app/index.html'))
 });
 
-app.listen(3000, function () {
+app.listen(3002, function () {
     console.log('Example app listening on port 3000!');
 });
 
